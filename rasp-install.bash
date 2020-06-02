d92383bc08f6f7b3351038e168d8c7114e329e07
Andrey Kuvshinov <delf@iMac-Andrey.local>
2020-06-02 18:15:15 +0300

    add rasp-install.bash

---
+++ rasp-install.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sudo apt-get install git
+
+echo "add php ..."
+sudo apt-get install -y php php-pdo php-mysql php-json php-xml php-gd php-zip
+
+echo "add nodejs"
+sudo apt-get install -y nodejs yarn -y
+
+
+
+
+
